0ef222fcb1257b8c17414bad5240bdc227c427fa
Jim Menard <jim.menard@chloeandisabel.com>
2016-01-29 20:58:00 -0500

    update

--- elixir/palindrome-products/palindrome_products.exs
+++ elixir/palindrome-products/palindrome_products.exs
@@ -5,15 +5,19 @@ defmodule Palindromes do
   """
   @spec generate(non_neg_integer, non_neg_integer) :: map() 
   def generate(max_factor, min_factor \\ 1) do
-    pals = %{}
-    for a <- (min_factor..max_factor),
-        b <- (min_factor..max_factor),
-        prod = a*b,
-        palindrome?(prod) do
-      pals = Map.put(pals, prod, [[a,b] | Map.get(pals, prod, [])])
-      IO.puts "pals is now #{inspect pals}"
-    end
-    pals
+    pals = for a <- (min_factor..max_factor),
+               b <- (min_factor..max_factor),
+               prod = a*b,
+               palindrome?(prod) do
+             {prod, [a, b]}
+           end
+    condense_map(pals, %{})
+  end
+
+  defp condense_map([], m), do: m
+  defp condense_map([{n, pair}|t], m) do
+    IO.puts "n #{n}, pair #{inspect pair}, t #{inspect t}, m #{inspect m}"
+    condense_map(t, Map.put(m, n, [Enum.sort(pair) | Map.get(m, n, [])]))
   end
 
   def palindrome?(n) do
