bcf5eb9c78167aa86efab60e05030ea3a368841d
Jim Menard <jim.menard@chloeandisabel.com>
2016-02-06 23:36:04 -0500

    update

--- clojure/queen-attack/src/queen_attack.clj
+++ clojure/queen-attack/src/queen_attack.clj
@@ -1,16 +1,18 @@
 (ns queen-attack)
 
 (defn board-string [{[w-row w-col] :w [b-row b-col] :b}]
-  (->>
-   (for [row (range 8)
-         col (range 8)]
-     (cond (and (= row w-row) (= col w-col)) \W
-           (and (= row b-row) (= col b-col)) \B
-           :true \-))
-   (interpose \space)
-   (apply str)
-   (interpose \n)
+  (let [board-chars (->>
+                     (for [row (range 8)
+                           col (range 8)
+                           :let [_ (println "row" row "col" col)]
+                           ]
+                       (cond (and (= row w-row) (= col w-col)) \W
+                             (and (= row b-row) (= col b-col)) \B
+                             :true \-)))]
+    (println (interpose \space board-chars))
+    (-> (conj (interpose \space board-chars) \space)
+        (partition 16))))
+))
+   (interpose \newline)
+   flatten
    (apply str)))
-
-
-
