2c7c58da0b090aaa9fa8573cf4ad62437416a7f9
Jim Menard <jim.menard@chloeandisabel.com>
2016-02-10 10:42:26 -0500

    update

--- haskell/bob/Bob.hs
+++ haskell/bob/Bob.hs
@@ -3,19 +3,24 @@ module Bob (responseFor) where
 import Data.Char
 -- problem: doesn't recognize Unicode space characters
 import Data.String.Utils (strip)
+import Text.Regex (mkRegex, subRegex)
 
 isYelling :: String -> Bool
-isYelling s = s == map toUpper s
+isYelling s = (s == map toUpper s) && (s /= map toLower s)
 
 isQuestion :: String -> Bool
 isQuestion s = '?' == last s
 
-isEmpty :: String -> Bool
-isEmpty s = length (strip s) == 0
+-- Borrowed from http://exercism.io/submissions/2f39f9c9d6f3110cf0961c19
+-- I tried figuring out how to create a Unicode regex that would match
+-- Unicode space characters, but gave up.
+isWhitespace :: String -> Bool
+isWhitespace =
+  all (`elem` "\n\r \t\v\xA0\x2002")
 
 responseFor :: String -> String
 responseFor s
-    | isEmpty s    = "Fine. Be that way!"
-    | isYelling s  = "Whoa, chill out!"
-    | isQuestion s = "Sure."
-    | otherwise    = "Whatever."
+  | isWhitespace s = "Fine. Be that way!"
+  | isYelling s    = "Whoa, chill out!"
+  | isQuestion s   = "Sure."
+  | otherwise      = "Whatever."
