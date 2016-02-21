34e48295327de8d12ad121bb88f42f4113e8b9a6
Jim Menard <jim@jimmenard.com>
2016-02-21 09:36:36 -0500

    update

--- go/say/say.go
+++ go/say/say.go
@@ -1,5 +1,6 @@
 package say
 
+import "fmt"					// DEBUG
 import "strings"
 
 var powers = []string{
@@ -14,22 +15,85 @@ var powers = []string{
 	"septillion",
 	"octillion",
 	"nonillion",
-	"decillion"
+	"decillion",
+}
+var digits = []string{
+	"",
+	"one",
+	"two",
+	"three",
+	"four",
+	"five",
+	"six",
+	"seven",
+	"eight",
+	"nine",
+}
+var teens = []string{
+	"ten",
+	"eleven",
+	"twelve",
+	"thirteen",
+	"fourteen",
+	"fifteen",
+	"sixteen",
+	"seventeen",
+	"eighteen",
+	"nineteen",
+}
+var tens = []string{
+	"",
+	"",
+	"twenty",
+	"thirty",
+	"forty",
+	"fifty",
+	"sixty",
+	"seventy",
+	"eighty",
+	"ninety",
 }
 
 func Say(n uint64) string {
 	if n == 0 {
 		return "zero"
 	}
-	words := make([]string)
+	words := make([]string, 0)
 	power := 0
-	while n > 0 {
+	for n > 0 {
+		fmt.Println("Say n", n)
 		words = sayWithPower(n % 100, powers[power], words)
+		n /= 100
 		power++
 	}
+	fmt.Println("finally, words before join", words)
 	return strings.Join(words, " ")
 }
 
-func sayWithPower(n uint, powerWord string, words []string) []string {
+func sayWithPower(n uint64, powerWord string, words []string) []string {
+	fmt.Println("sayWithPower", n, powerWord, words)
 	words = sayNumber(n, words)
+	if powerWord != "" {
+		words = append(words, powerWord)
+	}
+	return words
+}
+
+func sayNumber(n uint64, words []string) []string {
+	fmt.Println("sayNumber", n, words)
+	if n < 10 || n >= 20 {
+		word := ""
+		tensPlace := n / 10
+		if tens[tensPlace] != "" {
+			word += tens[tensPlace]
+		}
+		digit := n%10
+		if digit > 0 && tens[tensPlace] != "" {
+			word += "-" + digits[digit]
+		}
+		words = append(words, word)
+	} else {
+		words = append(words, teens[n-10])
+	}
+	return words
 }
