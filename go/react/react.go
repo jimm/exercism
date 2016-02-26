5c5f6ab6bbcb5566b9a7944f52d02816b6b65359
Jim Menard <jim.menard@chloeandisabel.com>
2016-02-25 20:01:26 -0500

    update

--- go/react/react.go
+++ go/react/react.go
@@ -4,9 +4,14 @@ const testVersion = 4
 
 var nextCallbackId = 0
 
-type JReactor struct{}
+type JReactor struct {
+	working bool
+	changed map[*JCell]bool
+}
 type JCell struct {
+	reactor   *JReactor
 	value     int
+	OrigValue interface{}
 	observers []*JCell
 	inputs    []*JCell
 	f         func([]*JCell) int
@@ -17,13 +22,13 @@ type JCallbackHandle struct {
 }
 
 func New() Reactor {
-	return new(JReactor)
+	return &JReactor{working: false}
 }
 
 // CreateInput creates an input cell linked into the reactor with the given
 // initial value.
 func (r *JReactor) CreateInput(v int) InputCell {
-	return &JCell{value: v}
+	return &JCell{reactor: r, value: v}
 }
 
 // CreateCompute1 creates a compute cell which computes its value based on
@@ -34,8 +39,8 @@ func (r *JReactor) CreateCompute1(v Cell, f func(int) int) ComputeCell {
 	wrapper := func(args []*JCell) int {
 		return f(args[0].Value())
 	}
-	c := &JCell{value: f(in.Value()), f: wrapper, inputs: []*JCell{in},
-		callbacks: make(map[int]func(int), 0)}
+	c := &JCell{reactor: r, value: f(in.Value()), f: wrapper,
+		inputs: []*JCell{in}, callbacks: make(map[int]func(int), 0)}
 	in.addObserver(c)
 	return c
 }
@@ -49,8 +54,8 @@ func (r *JReactor) CreateCompute2(v1 Cell, v2 Cell, f func(int, int) int) Comput
 	wrapper := func(args []*JCell) int {
 		return f(args[0].Value(), args[1].Value())
 	}
-	c := &JCell{value: f(in1.Value(), in2.Value()), f: wrapper, inputs: []*JCell{in1, in2},
-		callbacks: make(map[int]func(int), 0)}
+	c := &JCell{reactor: r, value: f(in1.Value(), in2.Value()), f: wrapper,
+		inputs: []*JCell{in1, in2}, callbacks: make(map[int]func(int), 0)}
 	in1.addObserver(c)
 	in2.addObserver(c)
 	return c
@@ -61,14 +66,58 @@ func (c JCell) Value() int {
 	return c.value
 }
 
-// SetValue sets the value of the cell.
+// SetValue sets the value of the cell. This isn't thread safe, but then
+// again the tests don't require that it be thread safe.
 func (c *JCell) SetValue(v int) {
+	r := c.reactor
+	wasWorking := r.working
+	if !wasWorking {
+		r.startWorking()
+	}
 	if c.value != v {
-		c.value = v
-		c.changed()
-		for _, f := range c.callbacks {
-			f(v)
+		c.saveValue(v)
+		r.addChangedCell(c)
+	}
+	if !wasWorking {
+		r.stopWorking()
+	}
+}
+
+func (r *JReactor) startWorking() {
+	r.working = true
+	r.changed = map[*JCell]bool{}
+}
+
+func (r *JReactor) addChangedCell(c *JCell) {
+	r.changed[c] = true
+}
+
+func (r *JReactor) stopWorking() {
+	for c, _ := range r.changed {
+		val := c.Value()
+		if val != c.OrigValue {
+			for _, f := range c.callbacks {
+				f(val)
+			}
 		}
+		c.OrigValue = nil
+	}
+	r.working = false
+}
+
+// saveValue sets the new value of the cell and notifies observers. If the
+// cell has not yet saved its original value, then do so.
+func (c *JCell) saveValue(v int) {
+	if c.OrigValue == nil {
+		c.OrigValue = c.value
+	}
+	c.value = v
+	c.notifyObservers()
+}
+
+func (c *JCell) notifyObservers() {
+	for _, o := range c.observers {
+		o.SetValue(o.f(o.inputs)) // recalculate
 	}
 }
 
@@ -89,10 +138,4 @@ func (c *JCell) RemoveCallback(cbh CallbackHandle) {
 func (v *JCell) addObserver(c ComputeCell) {
 	jc := c.(*JCell)
 	v.observers = append(v.observers, jc)
-}
-
-func (v *JCell) changed() {
-	for _, o := range v.observers {
-		o.SetValue(o.f(o.inputs)) // recalculate
-	}
 }
