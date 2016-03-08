3167260991ed19d033c96f9e0146f2120860992c
Jim Menard <jim.menard@chloeandisabel.com>
2016-03-08 07:23:13 -0500

    refactoring

--- go/simple-cipher/cipher.go
+++ go/simple-cipher/cipher.go
@@ -34,26 +34,11 @@ func NewShift(distance int) Cipher {
 }
 
 func (c shift) Encode(s string) string {
-	return shiftString(s, c.distance)
+	return munge(s, func() int { return c.distance })
 }
 
 func (c shift) Decode(s string) string {
-	return shiftString(s, -c.distance)
-}
-
-func shiftString(s string, distance int) string {
-	s = normalize(s)
-	encoded := make([]rune, len(s))
-	for i, r := range s {
-		ord := int(r) + distance
-		if ord > int('z') {
-			ord -= 26
-		} else if ord < int('a') {
-			ord += 26
-		}
-		encoded[i] = rune(ord)
-	}
-	return string(encoded)
+	return munge(s, func() int { return -c.distance })
 }
 
 // ================ Vigenere ================
@@ -74,29 +59,43 @@ func (c vigenere) Decode(s string) string {
 }
 
 func vigenereize(s, key string, direction int) string {
-	s = normalize(s)
-	encoded := make([]rune, len(s))
 	keyOffset := 0
-	for i, r := range s {
+	return munge(s, func() int {
 		distance := direction * (int(key[keyOffset]) - int('a'))
-		ord := int(r) + distance
-		if ord > int('z') {
-			ord -= 26
-		} else if ord < int('a') {
-			ord += 26
-		}
-		encoded[i] = rune(ord)
 		keyOffset++
 		if keyOffset >= len(key) {
 			keyOffset = 0
 		}
-	}
-	return string(encoded)
+		return distance
+	})
 }
 
 // ================ common ================
 
+// munge encodes/decodes s. For each rune in s, the distance func is called
+// to see how far to shift that rune.
+func munge(s string, distance func() int) string {
+	s = normalize(s)
+	encoded := make([]rune, len(s))
+	for i, r := range s {
+		encoded[i] = shiftRune(r, distance())
+	}
+	return string(encoded)
+}
+
+// normalize returns s stripped of all but a-z and made lower case.
 func normalize(s string) string {
 	re := regexp.MustCompile("[^a-z]+")
 	return string(re.ReplaceAll([]byte(strings.ToLower(s)), []byte("")))
+}
+
+// shiftRune shifts r by distance and clamps it within the range ['a', 'z'].
+func shiftRune(r rune, distance int) rune {
+	ord := int(r) + distance
+	if ord > int('z') {
+		ord -= 26
+	} else if ord < int('a') {
+		ord += 26
+	}
+	return rune(ord)
 }
