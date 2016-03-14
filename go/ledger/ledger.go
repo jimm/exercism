e367a06c563696cfe78642e87986acf2578e5a18
Jim Menard <jim.menard@chloeandisabel.com>
2016-03-14 07:36:22 -0400

    Check for empty entries before copying

--- go/ledger/ledger.go
+++ go/ledger/ledger.go
@@ -15,14 +15,14 @@ type Entry struct {
 }
 
 func FormatLedger(currency string, locale string, entries []Entry) (string, error) {
-	var entriesCopy []Entry
-	for _, e := range entries {
-		entriesCopy = append(entriesCopy, e)
-	}
 	if len(entries) == 0 {
 		if _, err := FormatLedger(currency, "en-US", []Entry{{Date: "2014-01-01", Description: "", Change: 0}}); err != nil {
 			return "", err
 		}
+	}
+	var entriesCopy []Entry
+	for _, e := range entries {
+		entriesCopy = append(entriesCopy, e)
 	}
 	m1 := map[bool]int{true: 0, false: 1}
 	m2 := map[bool]int{true: -1, false: 1}
